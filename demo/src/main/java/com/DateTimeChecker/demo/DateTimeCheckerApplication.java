3cbe45d1282c8d11d7650a408919127f6c1e8579
Ho Gia Bao Huy <hohuytencent@gmail.com>
2025-09-25 14:38:46 +0700

    commit

---
+++ demo/src/main/java/com/DateTimeChecker/demo/DateTimeCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.DateTimeChecker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTimeCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateTimeCheckerApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/DateTimeChecker/demo/DateTimeCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DateTimeChecker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
